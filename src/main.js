761695f1aba41df2f2eda9ae2fffea2423f2decc
Kamedin <dev@dev.dev>
2024-01-21 09:05:10 +0100

    added logo to project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
+
+// ASSETS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.mount('#app')
