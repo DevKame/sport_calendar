acef5813477438290bb975cda6cd0b6b2c9aff60
Kamedin <dev@dev.dev>
2024-01-30 17:13:49 +0100

    thinned fa implementation

--- src/main.js
+++ src/main.js
@@ -21,37 +21,14 @@ import InfoBox              from "./comps/dashboard/shared/InfoBox.vue";
 import { library }              from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon }      from "@fortawesome/vue-fontawesome";
 
-import { faBarsStaggered }      from "@fortawesome/free-solid-svg-icons";
-import { faMicrochip }          from "@fortawesome/free-solid-svg-icons";
-import { faClose }              from "@fortawesome/free-solid-svg-icons";
-import { faRectangleList }      from "@fortawesome/free-regular-svg-icons";
-import { faDumbbell }           from "@fortawesome/free-solid-svg-icons";
-import { faPersonChalkboard }   from "@fortawesome/free-solid-svg-icons";
-import { faPeopleGroup }        from "@fortawesome/free-solid-svg-icons";
-import { faTable }              from "@fortawesome/free-solid-svg-icons";
-import { faGear }               from "@fortawesome/free-solid-svg-icons";
-import { faPencil }             from "@fortawesome/free-solid-svg-icons";
-import { faBan }                from "@fortawesome/free-solid-svg-icons";
-import { faUserSlash }          from "@fortawesome/free-solid-svg-icons";
-import { faCalendar }           from "@fortawesome/free-regular-svg-icons";
-import { faBackward }           from "@fortawesome/free-solid-svg-icons";
-import { faForward }            from "@fortawesome/free-solid-svg-icons";
+//  -- IMPORT NEEDED ICONS
+import { faBarsStaggered,faMicrochip,faClose,faDumbbell,faPersonChalkboard,faPeopleGroup,faTable,faGear,faPencil,faBan,faUserSlash,faBackward,faForward }
+from "@fortawesome/free-solid-svg-icons";
+//  -- AND ADD THEM TO LIBRARY
+import { faRectangleList, faCalendar }
+from "@fortawesome/free-regular-svg-icons";
 
-library.add(faMicrochip);
-library.add(faClose);
-library.add(faBarsStaggered);
-library.add(faRectangleList);
-library.add(faDumbbell);
-library.add(faPersonChalkboard);
-library.add(faPeopleGroup);
-library.add(faTable);
-library.add(faGear);
-library.add(faPencil);
-library.add(faBan);
-library.add(faUserSlash);
-library.add(faCalendar);
-library.add(faBackward);
-library.add(faForward);
+library.add(faMicrochip,faClose,faBarsStaggered,faRectangleList,faDumbbell,faPersonChalkboard,faPeopleGroup,faTable,faGear,faPencil,faBan,faUserSlash,faCalendar,faBackward,faForward);
 
 // ROUTER
 import router from "./router/router.js";
